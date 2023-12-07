611c64a3cf634cbed0433d72030f48cf957c6f11
Shin Donghwan(신동환) <God.DongHwan@gmail.com>
2023-12-07 13:27:47 +0900

    [level unrated] Title: 주사위 게임 2, Time: 0.01 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.py
+++ 프로그래머스/unrated/181930. 주사위 게임 2/주사위 게임 2.py
@@ -1,6 +1,6 @@
 def solution(a, b, c):
     answer = 1
-    cnt = 1
+
     if 1 > a or 6 < a:
         print(f"a value Error : {a}")
         return -1
@@ -11,14 +11,17 @@ def solution(a, b, c):
         print(f"c value Error : {c}")
         return -1
     
-    if a == b:
-        cnt += 1
-        if b == c:
-            cnt += 1
-    elif b == c or a == c:
-        cnt += 1
-
+#    cnt = 1
+#    if a == b:
+#        cnt += 1
+#        if b == c:
+#            cnt += 1
+#    elif b == c or a == c:
+#        cnt += 1
+#
+#    for i in range(1,cnt+1):
+#        answer *= a**i+b**i+c**i
+    cnt = 4 - len(set([a,b,c]))
     for i in range(1,cnt+1):
-        answer *= a**i+b**i+c**i
-        
+        answer *= a**i+b**i+c**i    
     return answer
