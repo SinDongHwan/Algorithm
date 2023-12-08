823a1b9811cd3f59e04e492d411d0daec41de8b8
Shin Donghwan(신동환) <God.DongHwan@gmail.com>
2023-12-08 13:22:33 +0900

    [level unrated] Title: 마지막 두 원소, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.py
+++ 프로그래머스/unrated/181927. 마지막 두 원소/마지막 두 원소.py
@@ -4,13 +4,18 @@ def solution(num_list):
         print(f"num_list length Error : {len(num_list)}")
         return -1
     
-    for idx, num in enumerate(num_list):
-        answer.append(num)
-        
-        if idx == len(num_list)-1:
-            if num > num_list[idx-1]:
-                answer.append(num-num_list[idx-1])
-            else:
-                answer.append(num*2)
+#    for idx, num in enumerate(num_list):
+#        answer.append(num)
+#        
+#        if idx == len(num_list)-1:
+#            if num > num_list[idx-1]:
+#                answer.append(num-num_list[idx-1])
+#            else:
+#                answer.append(num*2)
                 
+    if num_list[-2] < num_list[-1]:
+        num_list.append(num_list[-1] - num_list[-2])
+    else:
+        num_list.append(num_list[-1]*2)
+    answer = num_list
     return answer
