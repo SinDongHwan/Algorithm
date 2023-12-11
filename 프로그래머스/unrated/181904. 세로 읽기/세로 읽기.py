cf21a9091e0d279397d313077a85997843750049
Shin Donghwan(신동환) <God.DongHwan@gmail.com>
2023-12-12 02:19:29 +0900

    [level unrated] Title: 세로 읽기, Time: 0.00 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.py
+++ 프로그래머스/unrated/181904. 세로 읽기/세로 읽기.py
@@ -1,5 +1,5 @@
 def solution(my_string, m, c):
     answer = ''
-    answer = "".join([my_string[idx] for idx in range(c-1,len(my_string),m)])
-
+    # answer = "".join([my_string[idx] for idx in range(c-1,len(my_string),m)])
+    answer = my_string[c-1::m] # c-1 ~ len(my_string), m번째 것만 추출
     return answer
