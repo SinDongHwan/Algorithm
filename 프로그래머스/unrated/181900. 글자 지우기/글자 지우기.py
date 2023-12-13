8fe55c214b9927e795f439d3735c53a141bce62d
Shin Donghwan(신동환) <God.DongHwan@gmail.com>
2023-12-13 19:31:24 +0900

    [level unrated] Title: 글자 지우기, Time: 0.02 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.py
+++ 프로그래머스/unrated/181900. 글자 지우기/글자 지우기.py
@@ -1,8 +1,13 @@
 def solution(my_string, indices):
     answer = ''
     my_string = list(my_string)
+    # for idx in indices:
+    #     my_string.pop(idx)
+    #     my_string.insert(idx, '0')
+    # answer = "".join(my_string).replace('0','')
+    
+    indices.sort(reverse=True)
     for idx in indices:
-        my_string.pop(idx)
-        my_string.insert(idx, '0')
-    answer = "".join(my_string).replace('0','')
+        del my_string[idx]
+    answer = "".join(my_string)
     return answer
