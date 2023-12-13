d33ee0a0ab4c7255f09331d2cb9ef4e9036dae94
Shin Donghwan(신동환) <God.DongHwan@gmail.com>
2023-12-13 19:40:18 +0900

    [level unrated] Title: 가까운 1 찾기, Time: 0.00 ms, Memory: 10 MB -BaekjoonHub

--- 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.py
+++ 프로그래머스/unrated/181898. 가까운 1 찾기/가까운 1 찾기.py
@@ -1,7 +1,13 @@
 def solution(arr, idx):
     answer = 0
-    if arr[idx:].count(1) == 0:
+#     if arr[idx:].count(1) == 0:
+#         answer = -1
+#     else:
+#         answer = arr[idx:].index(1) + len(arr[:idx])
+        
+    try:
+        answer = arr.index(1, idx)
+    except:
         answer = -1
-    else:
-        answer = arr[idx:].index(1) + len(arr[:idx])
+        
     return answer
