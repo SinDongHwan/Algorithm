16e4246f2f9e575834860636ceb3a2435626adf3
Shin Donghwan(신동환) <God.DongHwan@gmail.com>
2023-12-13 19:07:57 +0900

    [level unrated] Title: 배열 만들기 1, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.py
+++ 프로그래머스/unrated/181901. 배열 만들기 1/배열 만들기 1.py
@@ -1,4 +1,5 @@
 def solution(n, k):
     answer = []
-    answer = [i for i in range(1,n+1) if i%k == 0]
+#    answer = [i for i in range(1,n+1) if i%k == 0]
+    answer = [i for i in range(k,n+1,k)]
     return answer
