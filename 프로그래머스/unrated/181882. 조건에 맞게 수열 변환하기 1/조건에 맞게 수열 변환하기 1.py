234efce23b51d936900f550602254bc2a94889ab
Shin Donghwan(신동환) <God.DongHwan@gmail.com>
2023-12-15 20:15:41 +0900

    [level unrated] Title: 조건에 맞게 수열 변환하기 1, Time: 12.22 ms, Memory: 16.5 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181882. 조건에 맞게 수열 변환하기 1/조건에 맞게 수열 변환하기 1.py
@@ -0,0 +1,10 @@
+def solution(arr):
+    answer = []
+    for element in arr:
+        if element >= 50 and element%2==0:
+            answer.append(element//2)
+        elif element < 50 and element%2:
+            answer.append(element*2)
+        else:
+            answer.append(element)
+    return answer
