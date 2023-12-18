216fc2f7804b177ed1096f3732a13645f8f57740
Shin Donghwan(신동환) <God.DongHwan@gmail.com>
2023-12-18 17:09:47 +0900

    [level unrated] Title: 문자열이 몇 번 등장하는지 세기, Time: 0.03 ms, Memory: 10.1 MB -BaekjoonHub

--- 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.py
+++ 프로그래머스/unrated/181871. 문자열이 몇 번 등장하는지 세기/문자열이 몇 번 등장하는지 세기.py
@@ -1,6 +1,8 @@
 def solution(myString, pat):
     answer = 0
-    for i in range(0,len(myString)):
+    for i in range(len(myString)):
+        if len(myString) - i < len(pat):
+            break
         if myString[i:i+len(pat)] == pat:
             answer += 1
     return answer
