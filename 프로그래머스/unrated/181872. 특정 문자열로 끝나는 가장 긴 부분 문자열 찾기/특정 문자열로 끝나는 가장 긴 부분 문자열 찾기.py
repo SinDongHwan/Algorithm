0c60549bf5c35135deb240fc459f025027af9b6b
Shin Donghwan(신동환) <God.DongHwan@gmail.com>
2023-12-18 17:03:21 +0900

    [level unrated] Title: 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기, Time: 0.00 ms, Memory: 10.2 MB -BaekjoonHub

--- 프로그래머스/unrated/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.py
+++ 프로그래머스/unrated/181872. 특정 문자열로 끝나는 가장 긴 부분 문자열 찾기/특정 문자열로 끝나는 가장 긴 부분 문자열 찾기.py
@@ -1,5 +1,8 @@
 def solution(myString, pat):
     answer = ''
-    last_index = myString[::-1].index(pat[::-1])
-    answer = myString[:len(myString)-last_index]
+#    last_index = myString[::-1].index(pat[::-1])
+#    answer = myString[:len(myString)-last_index]
+    
+    last_index = myString.rindex(pat)
+    answer = myString[:last_index+len(pat)]
     return answer
