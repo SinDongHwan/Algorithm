7e4ce0318865d15bdb68715a4df08aaac8917fab
Shin Donghwan(신동환) <God.DongHwan@gmail.com>
2023-12-22 00:54:11 +0900

    [level unrated] Title: l로 만들기, Time: 3.53 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181834. l로 만들기/l로 만들기.py
+++ 프로그래머스/unrated/181834. l로 만들기/l로 만들기.py
@@ -1,6 +1,6 @@
 def solution(myString):
     answer = ''
-    for alpha in myString:
-        answer += 'l' if alpha < 'l' else alpha
-        
+    # for alpha in myString:
+    #     answer += 'l' if alpha < 'l' else alpha
+    answer = "".join(['l' if alpha < 'l' else alpha for alpha in myString])  
     return answer
