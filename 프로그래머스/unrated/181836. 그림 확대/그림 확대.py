d9873683ee517c1438a0f35a1c169894247c2b9a
Shin Donghwan(신동환) <God.DongHwan@gmail.com>
2023-12-22 00:44:22 +0900

    [level unrated] Title: 그림 확대, Time: 0.01 ms, Memory: 10.3 MB -BaekjoonHub

--- 프로그래머스/unrated/181836. 그림 확대/그림 확대.py
+++ 프로그래머스/unrated/181836. 그림 확대/그림 확대.py
@@ -1,10 +1,14 @@
 def solution(picture, k):
     answer = []
     
-    for col in picture:
-        pixel = ''
-        for row in col:
-            pixel += row*k
-        for i in range(k):
-            answer.append(pixel)
+    # for col in picture:
+    #     pixel = ''
+    #     for row in col:
+    #         pixel += row*k
+    #     for i in range(k):
+    #         answer.append(pixel)
+    
+    for idx in range(len(picture)):
+        for _ in range(k):
+            answer.append(picture[idx].replace('.', '.'*k).replace('x', 'x'*k))
     return answer
