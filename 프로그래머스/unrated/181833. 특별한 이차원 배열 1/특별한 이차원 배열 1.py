1691ba62e291c8e1fe537ee17122f962ee8447b8
Shin Donghwan(신동환) <God.DongHwan@gmail.com>
2023-12-22 01:14:12 +0900

    [level unrated] Title: 특별한 이차원 배열 1, Time: 0.06 ms, Memory: 10.5 MB -BaekjoonHub

--- 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
+++ 프로그래머스/unrated/181833. 특별한 이차원 배열 1/특별한 이차원 배열 1.py
@@ -1,11 +1,15 @@
 def solution(n):
     answer = [[]]
+    # for i in range(n):
+    #     if len(answer) == i :
+    #         answer.append([])
+    #     for j in range(n):
+    #         if i == j:
+    #             answer[i].append(1)
+    #         else:
+    #             answer[i].append(0)
+    
+    answer = [[0] * n for _ in range(n)]
     for i in range(n):
-        if len(answer) == i :
-            answer.append([])
-        for j in range(n):
-            if i == j:
-                answer[i].append(1)
-            else:
-                answer[i].append(0)
+        answer[i][i] = 1
     return answer
