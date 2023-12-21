10100c920f6ad848efd363ecba39d9001be74c19
Shin Donghwan(신동환) <God.DongHwan@gmail.com>
2023-12-22 00:32:15 +0900

    [level unrated] Title: 커피 심부름, Time: 0.00 ms, Memory: 10.1 MB -BaekjoonHub

---
+++ 프로그래머스/unrated/181837. 커피 심부름/커피 심부름.py
@@ -0,0 +1,8 @@
+def solution(order):
+    answer = 0
+    for order_ in order:
+        if ("americano" in order_) or ("anything" in order_):
+            answer += 4500
+        elif "cafelatte" in order_:
+            answer += 5000
+    return answer
